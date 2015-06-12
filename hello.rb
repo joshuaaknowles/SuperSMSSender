03e6c9567262b9264a40cd21b37f4d042c5f1422
joshuaaknowles <joshuaaknowles@gmail.com>
2015-06-11 21:41:01 -0400

    finished beta

--- hello.rb
+++ hello.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
+require 'twilio-ruby'
 
 get '/' do
 	erb :form
+end
+
+post '/' do
+	account_sid = ENV['TWILIO_ACCOUNT_SID']
+	auth_token = ENV['TWILIO_AUTH_TOKEN']
+
+	@client = Twilio::REST::Client.new account_sid, auth_token 
+
+	@client.account.messages.create({
+   		 :from => '+18454434561',  
+   		 :to => params[:phonenumber], 
+  	 	 :body => params[:textmessage]  
+	})
+	return "sending '#{params[:textmessage]}' to #{params[:phonenumber]}"
 end
